0eeb886309fde44535018dc730a12fe90c185b97
ErikPerez312 <theerikperez@gmail.com>
2018-03-23 16:06:57 -0700

    Initial Commit

---
+++ EPModalStatus/EPModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  EPModalStatus.h
+//  EPModalStatus
+//
+//  Created by Erik Perez on 3/23/18.
+//  Copyright © 2018 Erik Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EPModalStatus.
+FOUNDATION_EXPORT double EPModalStatusVersionNumber;
+
+//! Project version string for EPModalStatus.
+FOUNDATION_EXPORT const unsigned char EPModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EPModalStatus/PublicHeader.h>
+
+
